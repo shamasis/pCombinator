6e6d62790ae72c5ea525c2ad004356ff352e1e55
Shamasis Bhattacharya <shamasis@fusioncharts.com>
2014-10-10 03:10:38 +0530

    Syntax reformatting as precursor to code documentation process of Lexer module.
    - The rewritten styling allows for encapsulation of prototype in an object as such more readable
    - The prototype can now have @lends property to Lex @constructor
    - Minor code refactor and lint fixes.
    
    Not attended: equality check casting issues.

--- lexer.js
+++ lexer.js
@@ -1,79 +1,83 @@
 /**
- * New node file
+ * @constructor
+ * @param {array} stream
  */
+var Lex = function (stream) {
+    this.stream = stream;
 
-exports.Lex= function (stream){
-	this.stream=stream;
-	this.pos=0;
-	this.line=0;
-	this.col=0;
-	this.lastToken=null;
-	this.skipWhiteSpaces();		
-	return this;
+    this.pos = 0;
+    this.line = 0;
+    this.col = 0;
+    this.lastToken = null;
+    this.skipWhiteSpaces();
+
+    return this;
 };
 
-exports.Lex.prototype.skipWhiteSpaces=function(){
-	while(this.pos<this.stream.length){
-		var c=this.stream.charAt(this.pos);
-		if(c==' ' || c=='\t' || c=='\r' || c=='\n'){
-			if(c=='\n'){
-				this.line++;
-				this.col=0;
-				}
-			this.pos++;
-			}
-		else break;
-	}	
-	return this;
-}
+Lex.prototype = /** @lends Lex.prototype */ {
+	constructor: Lex,
 
-exports.Lex.prototype.advance=function(by){
-	var inc=by || 1;
-	this.pos+=inc;
-	this.skipWhiteSpaces();	
-	return this;
+    skipWhiteSpaces: function () {
+        var c;
+
+        while (this.pos < this.stream.length) {
+            c = this.stream.charAt(this.pos);
+            if (c == ' ' || c == '\t' || c == '\r' || c == '\n') {
+                if (c == '\n') {
+                    this.line++;
+                    this.col = 0;
+                }
+                this.pos++;
+            }
+            else {
+                break;
+            }
+        }
+        return this;
+    },
+
+    advance: function (by) {
+        this.pos += (by || 1);
+        this.skipWhiteSpaces();
+        return this;
+    },
+
+    matchFromList: function (list) {
+        var remaining = this.stream.slice(this.pos),
+            result = false,
+
+            i;
+
+        for (i = 0; i < list.length; i++) {
+            if (remaining.substr(0, list[i].length) == list[i]) {
+                result = list[i];
+                break;
+            }
+        }
+
+        return result;
+    },
+
+    getToken: function () {
+        this.lastToken = null;
+
+        if (this.pos < this.stream.length) {
+            this.lastToken = this.stream.charAt(this.pos);
+        }
+
+        return this.lastToken;
+    },
+
+    beginsWith: function (rx) {
+        var match = this.stream.slice(this.pos).match(rx);
+        return (match && match[0]) || false;
+    },
+
+    getPos: function () {
+        return this.pos;
+    }
 };
 
-exports.Lex.prototype.matchFromList=function (list){
-	var remaining=this.stream.slice(this.pos);
-	var result=false;
-	for(var i=0;i<list.length;i++){
-		if(remaining.substr(0,list[i].length)==list[i]){
-			result=list[i];
-			break;
-		}
-	}
-	 
-	return result;
+module.exports = {
+	Lex: Lex
 };
-
-exports.Lex.prototype.getToken= function (){
-	this.lastToken=null;
- 
-	if(this.pos<this.stream.length)
-		this.lastToken=this.stream.charAt(this.pos);
-	 
-	return this.lastToken;
-};
-
-exports.Lex.prototype.beginsWith=function(rx){
-	var remaining=this.stream.slice(this.pos);
-	var result=false;
-	var match=remaining.match(rx); 
-	
-	result=(match && match[0]) || false;
-	return result;
-};
-
-exports.Lex.prototype.getPos=function(){
-	return this.pos;
-}
-
- 
-
-
-
-
-
-
-	 
